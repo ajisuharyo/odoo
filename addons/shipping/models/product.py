fdab1a9e155422fa63fbe03cb9228417188b696f
Aji Suharyo <aji.suharyo@wgs.co.id>
2016-12-01 19:15:44 +0700

    basic kanban view & basic status bar

--- addons/shipping/models/product.py
+++ addons/shipping/models/product.py
@@ -7,7 +7,7 @@ class product(models.Model):
 	_name = 'shipping.products' #) nama ini akan menjadi nama tabel, bisa di relasikan dengan model lain. eg: shipping.route
 	_rec_name = 'name' #) ini untuk breadcumb
 	_description = "Master data Produk Penyiapan Jalur"
-	_sql_constraints = [('code_uniq', 'unique(code)', "Judul Harus Uniq Bro !!")]
+	_sql_constraints = [('code_uniq', 'unique(code)', "Kode Product Musti Beda !!")]
 
 	name = fields.Char(string='Nama Produk', required=True, select=1, index=True)
 	code = fields.Char(string='Kode Produk', help='buat kolom kode bro..')
@@ -16,10 +16,26 @@ class product(models.Model):
 	default_discount = fields.Float(digits=(10, 5), default='0')
 	last_purchase_price = fields.Float(digits=(10, 5))
 	point = fields.Integer(string='Poin', default='1')
-	type_product = fields.Selection([('Persediaan', 'Persediaan'), ('Aset', 'Aset'), ('Meneketehe', 'Meneketehe')], 'Type Produk')
+	type_product = fields.Selection([('Persediaan', 'Persediaan'), ('Aset', 'Aset'), ('Meneketehe', 'Meneketehe')], 'Type Produk', default='Persediaan')
 	status = fields.Boolean(string='Status', default=True, help='delete udah di hapus bearti')
 	description = fields.Text(string='Keterangan', help='ini buat isi keterangan') #) help bila di hover, akan memunculkan isi message nya
+	image = fields.Binary("Image", attachment=True) #) jika attachment true maka tidak akan membuat field image di tabel product, tetapi akan menggunakan relasi tabel sebagai lokasi menyimpan image (default ke tabel ir.attachment --> belum tau kenapa kesini dan cara merubahnya), jika attachment false maka akan membuat field image di tabel product
+	image_small = fields.Binary("Small-sized image", attachment=True) #) harus nya ini jadi image ukuran medium & kecil, tapi belum tau caranya, masih pabeulit manggil fungsinya
+	image_medium = fields.Binary("Medium-sized image", attachment=True)
+	state = fields.Selection([('concept', 'Concept'),  ('started', 'Started'), ('progress', 'In progress'), ('finished', 'Done'), ], default='concept')
 
+	@api.one #) mungkin @api.one harus di tulis di setiap fungsi nya
+	def concept_progressbar(self):
+		self.write({'state': 'concept'})
 
+	@api.one
+	def started_progressbar(self):
+		self.write({'state': 'started'})
 
+	@api.one
+	def progress_progressbar(self):
+		self.write({'state': 'progress'})
 
+	@api.one
+	def done_progressbar(self):
+		self.write({'state': 'finished'})
